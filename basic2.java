54b6610458fdc319d8b47d794d236551b71fab65
prasanth-17-byte <prasanth17082004@gmail.com>
2025-01-28 10:27:28 +0530

    Add files via upload

---
+++ basic2.java
@@ -0,0 +1,9 @@
+public  class basic2{
+    public static void main(String[] args){
+        int container=450;
+        String kumar="K";
+        String container1 =kumar;
+        System.out.println(container);
+        System.out.println(container1);
+    }
+}  
