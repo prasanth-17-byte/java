ad6b27d8b00d38931e8ab19a7a8f690f8b933c48
prasanth-17-byte <prasanth17082004@gmail.com>
2025-01-28 11:42:01 +0530

    Add files via upload

---
+++ basicsub.java
@@ -0,0 +1,9 @@
+public class basic5 {
+    public static void main(String[] args){
+        int a=4;
+        int b=6;
+        int c=1;
+        int d=a-b-c;
+        System.out.println(d);
+    }
+}
