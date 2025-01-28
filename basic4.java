88c4cf39424bc094de57d7cc857229028ceb8e16
prasanth-17-byte <prasanth17082004@gmail.com>
2025-01-28 11:31:31 +0530

    Add files via upload

---
+++ basic4.java
@@ -0,0 +1,9 @@
+public class basic4 {
+    public static void main(String[] args){
+        int a=4;
+        int b=6;
+        int c=15;
+        int d=a+b+c;
+        System.out.println(d);
+    }
+}
