5121459cbd6c85efd5ef54c39c9c88874d9401cd
Prasanth M <prasanth17082004@gmail.com>
2025-01-29 10:33:23 +0530

    Add files via upload

---
+++ code1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class code1{
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+        System.out.print("Enter a number :");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if(number < 5){
+        System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+    
+}    
---
+++ condition1.java
@@ -0,0 +1,15 @@
+public class condition1 
+{
+       public static void main(String[] args)
+{
+boolean rain = false;
+if(rain )
+{
+    System.out.println("Take an umbrella");
+}
+else
+{
+    System.out.println("Enjoy The Sunlight");
+}
+}
+}
