9ea19b045ee381f1498d2a28cca7b5c89741c712
Prasanth M <prasanth17082004@gmail.com>
2025-01-29 09:22:48 +0530

    Add files via upload

---
+++ datatypeoperation.java
@@ -0,0 +1,22 @@
+public class datatypeoperation{
+ public static void main(String[] args){
+    int a=10;
+    int b = 5;
+    System.out.println("\nRelational Operations:");
+    System.out.println("a > b"+(a>b));
+    System.out.println("a < b"+(a<b));
+    System.out.println("a <= b"+(a<=b));
+    System.out.println("a == b"+(a==b));
+    System.out.println("a != b"+(a!=b)); 
+    boolean x =true, y = false;
+    System.out.println("\nLogical Operations:");
+    System.out.println("x && y:"+(x && y));
+    System.out.println("x || y:"+(x || y));
+    System.out.println("!x:"+(!x));
+    int d = 5;
+    System.out.println("\nUninary Operations:");
+    System.out.println("++d:"+(++d));
+    System.out.println("d++:"+(d++));
+    System.out.println("d:"+d);
+ }  
+}
